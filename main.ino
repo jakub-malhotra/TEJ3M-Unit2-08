656f699d348d35db33b2622cf61f4aefd163eb9d
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-11-06 11:09:37 -0500

    november 6th

--- main.ino
+++ main.ino
@@ -1,3 +1,21 @@
-// Servo spins based on variable resistor
-// Created by Jakub Malhotra
-// Created on October 31st 2023
+//Uses a potentiometer to determine how a servo spins
+//Created by Jakub Malhotra
+//Created on November 2023
+
+#include <Servo.h>
+
+Servo servoOne; 
+
+int potPin = 0;
+int value;   
+
+void setup() {
+    servoOne.attach(9);
+}
+
+void loop() {
+    value = analogRead(potPin); 
+    value = map(value, 0, 1023, 0, 180);     
+    servoOne.write(value);                  
+    delay(15);                           
+}
